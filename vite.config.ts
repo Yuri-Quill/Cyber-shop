9ebc90bb09b9fcfdc0bc8a4985714f65231894c0
Yuri Quill <yury.fortis@gmail.com>
2025-04-26 05:50:53 +0300

    chore: update dependencies and configure Vite for HTTPS and SCSS support
    - Added vite-plugin-mkcert to enable HTTPS in development.
    - Updated Vite configuration to include HTTPS settings and SCSS preprocessor options.
    - Configured SCSS to automatically import color, variables, and mixins.
    - Added alias for the src directory for easier imports.
    - Updated devDependencies to include @types/node.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import mkcert from "vite-plugin-mkcert";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), mkcert()],
+    server: {
+		https: {},
+        host: 'localhost',
+        port: 5173,
+    },
+	css: {
+		postcss: "./postcss.config.js",
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+          @use "sass:color";
+          @use "@/helpers/variables" as *;
+          @use "@/helpers/mixins" as *;
+        `,
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});
