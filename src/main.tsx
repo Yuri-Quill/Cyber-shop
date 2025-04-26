e3cab0d78debd50ca3293654b8d879508253a4cd
Yuri Quill <yury.fortis@gmail.com>
2025-04-26 06:36:11 +0300

    feat: update dev script to open browser on start and implement routing in main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+import router from "./router";
 
 import "./config/axios.config";
 
@@ -9,4 +12,8 @@ import "react-toastify/dist/ReactToastify.css";
 import "./fonts/fonts.scss";
 import "./index.scss";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
